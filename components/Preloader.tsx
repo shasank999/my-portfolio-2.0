8ac063ba5d44fdf0a1173be3a85bf43fb61e5505
ShasankPurohit <shasankpurohit10@gmail.com>
2026-04-17 13:39:07 +0530

    Fix

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -7,61 +7,49 @@ gsap.registerPlugin(useGSAP);
 
 const Preloader = () => {
     const preloaderRef = useRef<HTMLDivElement>(null);
+    const text = 'WELCOME';
 
     useGSAP(
         () => {
             const tl = gsap.timeline({
-                defaults: {
-                    ease: 'power1.inOut',
-                },
+                defaults: { ease: 'power1.inOut' },
             });
 
-            tl.to('.name-text span', {
+            tl.to('.char', {
                 y: 0,
                 stagger: 0.05,
                 duration: 0.2,
-            });
-
-            tl.to('.preloader-item', {
-                delay: 1,
-                y: '100%',
-                duration: 0.5,
-                stagger: 0.1,
             })
-                .to('.name-text span', { autoAlpha: 0 }, '<0.5')
-                .to(
-                    preloaderRef.current,
-                    {
-                        autoAlpha: 0,
-                    },
-                    '<1',
-                );
+                .to('.preloader-item', {
+                    delay: 1,
+                    y: '100%',
+                    duration: 0.5,
+                    stagger: 0.1,
+                })
+                .to('.char', { autoAlpha: 0 }, '<0.5')
+                .to(preloaderRef.current, { autoAlpha: 0 }, '<1');
         },
         { scope: preloaderRef },
     );
 
     return (
         <div className="fixed inset-0 z-[6] flex" ref={preloaderRef}>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
-            <div className="preloader-item h-full w-[10%] bg-black"></div>
+            {Array.from({ length: 10 }).map((_, i) => (
+                <div
+                    key={i}
+                    className="preloader-item h-full w-[10%] bg-black"
+                />
+            ))}
 
-            <p className="name-text flex text-[20vw] lg:text-[200px] font-anton text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none overflow-hidden">
-                <span className="inline-block translate-y-full">T</span>
-                <span className="inline-block translate-y-full">A</span>
-                <span className="inline-block translate-y-full">J</span>
-                <span className="inline-block translate-y-full">M</span>
-                <span className="inline-block translate-y-full">I</span>
-                <span className="inline-block translate-y-full">R</span>
-                <span className="inline-block translate-y-full">U</span>
-                <span className="inline-block translate-y-full">L</span>
+            <p className="flex text-[20vw] lg:text-[200px] font-anton text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none overflow-hidden">
+                {text.split('').map((char, i) => (
+                    <span
+                        key={i}
+                        className="char inline-block translate-y-full"
+                    >
+                        {char}
+                    </span>
+                ))}
             </p>
         </div>
     );
